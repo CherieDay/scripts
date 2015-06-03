8d8142fd271267cb83498ddbe48a3fe7497926bd
Cherie Day <TheCreator@Cheries-MacBook-Pro.local>
2015-06-03 16:56:10 -0700

    Mean/Median Sigma Opt
    Added the option to do RFI flagging using either the standard definition of the standard deviation or replacing the mean with median in the calculation of the standard deviation. Also added user ability to specify tolerance level for number of sigma the data values must be less than.

--- rfi_flag.py
+++ rfi_flag.py
@@ -9,10 +9,23 @@
 #(2) Once I have the variance, do a where value is larger then 6 sigma, add it to the
 #		flags in the uv file.
 #Note, since I am using the median subtracted files, the offset from zero is zero.
+#How does file length (i.e. the length of time I'm finding the median over) affect 
+#the standard deviation I calculate? Also, how many outliers would I expect to see
+#at random given a Gaussian distribution?
 
-import numpy as np, aipy as a, sys
+import numpy as np, aipy as a, sys, optparse
 
-for uvfile in sys.argv[1:]:
+o = optparse.OptionParser()
+o.set_usage('rfi_flag.py [options] *.uv')
+o.set_description(__doc__)
+o.add_option('-m', '--mode', dest='mode', default='mean',
+    help='Use either the standard definition of variance and standard deviation or use the root median squared deviation from the median to calculate the standard deviation for RFI flagging. e.g -m median. The mean is the default.')
+o.add_option('-n', '--nsigma', dest='nsigma', default='5',
+	help='The number of sigma desired for the tolerance level. That is, all values greater than nsigma will be flagged. e.g -n 5. 5 is the default.')
+
+opts, args = o.parse_args(sys.argv[1:])
+
+for uvfile in args:
 	print uvfile
 	uv = a.miriad.UV(uvfile)
 	uv.select('antennae', 0,7, include=True) #Include only the cross-correlation
@@ -21,15 +34,21 @@ for uvfile in sys.argv[1:]:
 	data = np.array([d for p,d,f in uv.all(raw=True)], dtype=np.complex)
 
 	#Get the variance for each frequency channel
-	dvar = np.var(data, axis=0, dtype=np.complex)
+	sig_mean = np.sqrt(np.var(data, axis=0, dtype=np.complex))
+	sig_med = np.sqrt(np.median((abs(data-np.median(data,axis=0)))**2,axis=0))
 	
 	def rfiflag(uv, p, d, f):
-		f = np.where(abs(d)<6*dvar**(1/2.), f, False)
-		return p, d, f
+		n = float(opts.nsigma)
+		if opts.mode == 'mean': return p, d, np.where(abs(d)<n*sig_mean, f, True)
+		elif opts.mode == 'median': return p, d, np.where(abs(d)<n*sig_med, f, True)
+		else:  raise ValueError('Must specify either mean or median and/or nsigma.')
 
 	uv.rewind()
-	uvo = a.miriad.UV(uvfile+'R',status='new')
+	if opts.mode == 'mean':
+		uvo = a.miriad.UV(uvfile+'R',status='new')
+	elif opts.mode == 'median':
+		uvo = a.miriad.UV(uvfile+'RM', status='new')
 	uvo.init_from_uv(uv)
 	uvo.pipe(uv, mfunc=rfiflag, raw=True,
-		append2hist='''Flagged values in data greater then 6 sigma for
+		append2hist='''Flagged values in data greater then nsigma for
 		 each frequency channel for cross-correlation (0,7)\n''')
