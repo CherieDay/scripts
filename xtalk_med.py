36ff63c725f8871d4f4e63c0f2f395036aaf458a
Cherie Day <TheCreator@Cheries-MacBook-Pro.local>
2015-06-17 10:37:26 -0700

    Added Options
    Added the option to substract either the median or the mean from the data.

--- xtalk_med.py
+++ xtalk_med.py
@@ -1,8 +1,16 @@
 #! /usr/bin/env python
 
-import numpy as np, aipy as a, sys
+import numpy as np, aipy as a, sys, optparse
 
-for uvfile in sys.argv[1:]:
+o = optparse.OptionParser()
+o.set_usage('xtalk_med.py [options] *.uv')
+o.set_description(__doc__)
+o.add_option('-m', '--mode', dest='mode', default='median',
+    help='Subtract either the median or the mean from the data. The default is the median. e.g. -m median or -m mean.')
+
+opts, args = o.parse_args(sys.argv[1:])
+
+for uvfile in args:
 	print uvfile
 	uv = a.miriad.UV(uvfile)
 	uv.select('antennae', 0, 7, include=True)  #This tells it I want to include only the cross-correlations
@@ -19,11 +27,17 @@ for uvfile in sys.argv[1:]:
 	#Take the median of all the times in each frequency channel for real and imaginary
 	#components of data
 	med_data = np.median(data.real, axis=0) + 1j*np.median(data.imag, axis=0)
+	mean_data = np.mean(data.real, axis=0) + 1j*np.mean(data.imag, axis=0)
 	
-	def med(uv, p, d, f): return p, d-med_data, f
+	def med(uv, p, d, f):
+		if opts.mode == 'median': return p, d-med_data, f
+		elif opts.mode == 'mean': return p, d-mean_data, f
 
 	uv.rewind()
-	uvo = a.miriad.UV(uvfile+'m',status='new')
+	if opts.mode == 'median':
+		uvo = a.miriad.UV(uvfile+'m',status='new')
+	elif opts.mode == 'mean':
+		uvo = a.miriad.UV(uvfile+'me',status='new')
 	uvo.init_from_uv(uv)
 	uvo.pipe(uv, mfunc=med, raw=True,
 		append2hist="Subtracted median for each frequency channel for cross-correlation (0,7)\n")
